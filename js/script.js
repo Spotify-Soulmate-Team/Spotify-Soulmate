3c1c6b772aeddd7b206bb76ffc162dbf2a72eeb1
mhmdakoum <mohammedakoum0@gmail.com>
2024-03-21 21:51:37 +0200

    added back to home arrow and js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
-
-
 document.getElementById("artist-input").addEventListener("keypress", function(event) {
   if (event.keyCode === 13) {
     var userInput = document.getElementById("artist-input").value;
 
     window.location.href = "fav-artist.html?input=" + userInput;
   }
+});
+
+//for left arrow in favorite artist page
+document.getElementById("leftArrow").addEventListener("click", function() {
+  this.style.transform = "translateX(-100px)";
 });
