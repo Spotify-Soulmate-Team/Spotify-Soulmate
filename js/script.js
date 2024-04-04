ba5e4b865f48d0e9e5b5617dc88e653c3e1f959a
mhmdakoum <mohammedakoum0@gmail.com>
2024-04-04 09:09:17 +0300

    responsive hom-page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav-content");
+
+    hamburger.addEventListener("click", () => {
+      hamburger.classList.toggle("active");
+      navMenu.classList.toggle("active");
+    })
+
+    document.querySelectorAll(".nav-item").forEach(n => n.addEventListener("click", () => {
+      hamburger.classList.remove("active");
+      navMenu.classList.remove("active");
+    }))
+
 document.getElementById("artist-input").addEventListener("keypress", function(event) {
   if (event.keyCode === 13) {
     var userInput = document.getElementById("artist-input").value;
